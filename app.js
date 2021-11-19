7d94c5cad7fd178a1658f5ff7d1c9d51c324405e
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-11-19 15:51:39 +0000

    Simple express server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Task Manager App");
+const express = require("express");
+const tasks = require("./routes/tasks");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+const port = 5500;
+app.listen(port, console.log(`server is listening on port ${port}... `));
