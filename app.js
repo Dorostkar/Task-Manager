94d38dc3e2d768957d6b83ee8a5c7c5a14906882
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-11-20 13:12:14 +0000

    setup mongoose

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const connectDB = require("./db/connect");
 const express = require("express");
 const tasks = require("./routes/tasks");
+require("dotenv").config();
 
 const app = express();
 
@@ -12,5 +14,14 @@ app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
 
-const port = 5500;
-app.listen(port, console.log(`server is listening on port ${port}... `));
+const start = async () => {
+  const port = 5500;
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}... `));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
